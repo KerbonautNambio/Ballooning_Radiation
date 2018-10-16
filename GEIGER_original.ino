b1709a00bcea6a8490875e354fbd69a07e1eef53
KerbonautNambio <www.uhnambio@gmail.com>
2018-10-16 21:20:41 +0100

    Add files via upload

---
+++ GEIGER_original.ino
@@ -0,0 +1,70 @@
+// Conversion factor - CPM(counts per minute) to uSV/h(mircrosieverts per hour)
+#define CONV_FACTOR 0.00812
+
+// Variables
+int ledArray [] = {10,11,12,13,9};
+int geiger_input = 2;
+long count = 0;
+long countPerMinute = 0;
+long timePrevious = 0;
+long timePreviousMeassure = 0;
+long time = 0;
+long countPrevious = 0;
+float radiationValue = 0.0;
+
+void setup(){
+  pinMode(geiger_input, INPUT);
+  digitalWrite(geiger_input,HIGH);
+  for (int i=0;i<5;i++){
+    pinMode(ledArray[i],OUTPUT);
+  }
+
+void loop(){
+  if (millis()-timePreviousMeassure > 10000){
+    countPerMinute = 6*count;
+    radiationValue = countPerMinute * CONV_FACTOR;
+    timePreviousMeassure = millis();
+    Serial.print("cpm = "); 
+    Serial.print(countPerMinute,DEC);
+    Serial.print(" - ");
+    Serial.print("uSv/h = ");
+    Serial.println(radiationValue,4);      
+    
+    //led var setting  
+    if(countPerMinute <= TH1) ledVar(0);
+    if((countPerMinute <= TH2)&&(countPerMinute>TH1)) ledVar(1);
+    if((countPerMinute <= TH3)&&(countPerMinute>TH2)) ledVar(2);
+    if((countPerMinute <= TH4)&&(countPerMinute>TH3)) ledVar(3);
+    if((countPerMinute <= TH5)&&(countPerMinute>TH4)) ledVar(4);
+    if(countPerMinute>TH5) ledVar(5);
+
+    count = 0;
+
+  }
+
+}
+
+void countPulse(){
+  detachInterrupt(0);
+  count++;
+  while(digitalRead(2)==0){
+  }
+  attachInterrupt(0,countPulse,FALLING);
+}
+
+void ledVar(int value){
+  if (value > 0){
+    for(int i=0;i<=value;i++){
+      digitalWrite(ledArray[i],HIGH);
+    }
+    for(int i=5;i>value;i--){
+      digitalWrite(ledArray[i],LOW);
+    }
+  }
+  else {
+    for(int i=5;i>=0;i--){
+      digitalWrite(ledArray[i],LOW);
+    }
+  }
+}
+    
---
+++ UV_IR.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include "Adafruit_SI1145.h"
+
+Adafruit_SI1145 uv = Adafruit_SI1145();
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Adafruit SI1145 tested");
+  
+  if (! uv.begin()) {
+    Serial.println("Didn't find Si1145");
+    while (1);
+  }
+
+  Serial.println("OK, We are live!");
+}
+
+void loop() {
+  Serial.println("===================");
+  Serial.print("Visible UV: "); Serial.println(uv.readVisible());
+  Serial.print("IR: "); Serial.println(uv.readIR());
+}
